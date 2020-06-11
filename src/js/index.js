91e9e020dccacf123973b9855b50d84a3dfa3c7c
Danny Woodford <Daniel.woodford.g@gmail.com>
2020-06-11 00:41:28 -0400

    ui setup for welcome form and nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,11 @@
 const welcomeForm = document.querySelector(".welcome-form__page")
 
 document.addEventListener('click', (e) => {
+    console.dir(e.target)
     if(e.target.id === "submit"){
         e.preventDefault()
-        welcomeForm.classList.add("hide");
+        // setTimeout(() => {
+            welcomeForm.classList.add("hide");
+        // }, 600)
     }
 })
